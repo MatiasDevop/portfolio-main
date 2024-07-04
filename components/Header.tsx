f983ddb5d969367e3e4f5a60137a716e6e06354e
Matt <solomatias321@gmail.com>
2024-07-04 20:01:48 +0200

    wip navbar responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import React from "react";
 import Nav from "./Nav";
+import { Button } from "./ui/button";
 
 export const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
         {/* logo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
@@ -13,7 +14,15 @@ export const Header = () => {
           </h1>
         </Link>
         {/* desktop nav */}
-        <Nav />
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+
+        {/* mobile nav */}
+        <div className="xl:hidden">mobile nav</div>
       </div>
     </header>
   );
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,44 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
+
 const Nav = () => {
-  return <div> Desktop Nav</div>;
+  const pathname = usePathname();
+  console.log(pathname);
+  return (
+    <nav className="flex gap-8">
+      {links.map((link, index) => {
+        return (
+          <Link href={link.path} key={index} className={`$`}>
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
 };
 
 export default Nav;
