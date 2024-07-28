36c8d7f377ade602979a1f45939f56451ffa25b0
Matt <solomatias321@gmail.com>
2024-07-28 14:00:22 +0200

    wip: home page and socials

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import Social from "@/components/Social";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
 export default function Home() {
   return (
     <section className="h-full">
@@ -9,6 +13,24 @@ export default function Home() {
             <h1 className="h1">
               Hello I'm <br /> <span>Luke Coleman</span>
             </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant digital experiences and i am
+              proficient in vaious programing languages and technologies
+            </p>
+            {/* btn and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social />
+              </div>
+            </div>
           </div>
           <div>photo</div>
         </div>
---
+++ components/Social.tsx
@@ -0,0 +1,15 @@
+import path from "path";
+import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "" },
+  { icon: <FaLinkedinIn />, path: "" },
+  { icon: <FaYoutube />, path: "" },
+  { icon: <FaTwitter />, path: "" },
+];
+
+const Social = ({ containerStyles, iconStyles }) => {
+  return <div className={containerStyles}>Social</div>;
+};
+
+export default Social;
