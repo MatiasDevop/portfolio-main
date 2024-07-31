921ccd86a90ca20f9ee616553a2d1736657002a8
Matt <solomatias321@gmail.com>
2024-07-31 20:01:36 +0200

    wip: add social and photo comp

--- components/Social.tsx
+++ components/Social.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import path from "path";
 import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
 
@@ -8,8 +9,24 @@ const socials = [
   { icon: <FaTwitter />, path: "" },
 ];
 
-const Social = ({ containerStyles, iconStyles }) => {
-  return <div className={containerStyles}>Social</div>;
+const Social = ({
+  containerStyles,
+  iconStyles,
+}: {
+  containerStyles: any;
+  iconStyles: any;
+}) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Social;
