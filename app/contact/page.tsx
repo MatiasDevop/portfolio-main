a60e11aea930db55dc3057083daeec80a560288b
Matt <solomatias321@gmail.com>
2024-09-25 19:09:09 +0200

    wip: contact section

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -48,7 +48,35 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
           <div className="xl:h-[54%] xl:order-none">
-            <form>form</form>
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <p className="text-white/60">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Corrupti amet cumque quibusdam asperiores, id libero excepturi
+                nihil
+              </p>
+              {/* input */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="Firstname" />
+                <Input type="lastname" placeholder="Lastname" />
+                <Input type="email" placeholder="Email address" />
+                <Input type="phone" placeholder="Phone number" />
+              </div>
+              {/* select */}
+              <Select>
+                <SelectTrigger className="w-full ">
+                  <SelectValue placeholder="Select a service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectItem value="est">Web Development</SelectItem>
+                    <SelectItem value="cst">UI/UX design</SelectItem>
+                    <SelectItem value="mst">Logo Design</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </form>
           </div>
           {/* info */}
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
